d6e1cc039aa231e7c742deaa89e42452ff6d5ca5
Vadym Pechenoha <vad.pechenoga@gmail.com>
2016-05-12 22:33:02 +0300

    Main Class could be abstract

---
+++ Chapter 1/MainClassIsAbstract.java
@@ -0,0 +1,7 @@
+/* Main Class can be abstract */
+
+public abstract class MainClassIsAbstract {
+  public static void main(String[] args) {
+    System.out.println("Some Hello World string...");
+  }
+}
