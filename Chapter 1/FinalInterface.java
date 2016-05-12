e7495e61c3bc103c155b3a282d36e567cf73939b
Vadym Pechenoha <vad.pechenoga@gmail.com>
2016-05-12 22:02:45 +0300

    final keyword is not applicable for interface declaration

---
+++ Chapter 1/FinalInterface.java
@@ -0,0 +1,5 @@
+/* 
+  * Compilation error - an interface can't be final. 
+  * Each interface is abstract entity and its puprose is to be implemented. 
+  */
+/* final */ interface FinalInterface {}
