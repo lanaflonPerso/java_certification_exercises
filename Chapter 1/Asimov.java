eadb4d1c14eb43e8e7a3b21f28ce9bcea50e755c
Vadym Pechenoha <vad.pechenoga@gmail.com>
2016-05-12 21:56:38 +0300

    The name of file is not important if the class doesn't contain public class

---
+++ Chapter 1/Asimov.java
@@ -0,0 +1,6 @@
+/* The name of file is not important if the class doesn't contain public class */
+/* It could be equal to name of one of the classes in file or some foo name */
+class TheEndOfEthernity {}
+
+/* But if the file contains  Asimov public class, it should be named " Asimov.java" */
+/*public class Asimov {}*/
