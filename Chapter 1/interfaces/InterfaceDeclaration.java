95a80b80cbf7a1dfdd887301297110fd92491c59
Vadym Pechenoha <vad.pechenoga@gmail.com>
2016-05-14 03:46:53 +0300

    Interface declaration

---
+++ Chapter 1/interfaces/InterfaceDeclaration.java
@@ -0,0 +1,11 @@
+/* 
+ * As for classes, the name of public interface should be equal to the name of file, which contains it.
+ * The file should contain only 1 or 0 public interfaces.
+ */
+public interface InterfaceDeclaration {}
+
+/* 
+ * Interface is an abstract entity, so the 'abstract' word could be placed without any compilation errors.
+ * But there's no need for it, keep it simple :)
+ */
+abstract interface InterfaceWithExplicitAbstractModifier {}
