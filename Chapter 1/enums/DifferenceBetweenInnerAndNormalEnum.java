daed3057a0d8fe0dade3f09e86cb70c219e5e7ce
Vadym Pechenoha <vad.pechenoga@gmail.com>
2016-05-25 01:52:17 +0300

    Enum basic declaraton tests and differences between normal and inner enums

---
+++ Chapter 1/enums/DifferenceBetweenInnerAndNormalEnum.java
@@ -0,0 +1,42 @@
+public class DifferenceBetweenInnerAndNormalEnum {
+	/* Even inner enum can't be declared as final, but it can be declared as static */
+	private static /* final */ enum HomeAnimal {
+		DOG, CAT
+	}
+	
+	protected enum SemicolonCanBeUsed {
+		VAL_1, VAL_2
+	}; /* <-- */
+	
+	public static void main(String[] args) {
+		HomeAnimal homeAnimal = HomeAnimal.DOG;
+		System.out.println(homeAnimal);
+		
+		WildAnimal wildAnimal = WildAnimal.LION;
+		System.out.println(wildAnimal);
+		
+		/* Enum types MUST not be local, only normal or inner (inside class) */
+		/* enum EnumInsideMethodIsNotAllowed {VAL_1, VAL_2} */
+	}
+}
+
+/* The only possible modifier for normal (not inner) enums - 
+ * public access modifier (and absence of it - default access level) */
+/* static final */ enum WildAnimal {
+	WOLF, LION
+}
+
+/* Of course, enums can't be abstract (we can't extend it) */
+/* abstract */ enum Animal {Home, Wild}
+
+/* For declaration of normal (not inner) enum we can't use protected access level
+ * but we can do it for inner enums */
+/* protected */ enum TestEnumNotProtected {Home, Wild}
+
+interface SomeInterface {
+	/* Enum inside interface MUST not use final, protected modifiers */
+	/* protected final */ enum EnumInsideInterfaceIsAllowed {VAL_1, VAL_2}
+	
+	/* As for enums inside classe, enums inside interface can be marked as static */
+	static enum EnumInsideInterfaceCanBeStatic {VAL_1, VAL_2}
+}
